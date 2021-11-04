6c817c2fd87cd0d31a676b9e922ea314d303e37a
Kaiwen Tian <kaiwen.tian@wishlife.com>
2021-11-04 21:31:35 +0800

    feat:  added welcome screen.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:free_chat/screens/welcome/welcome_screen.dart';
 import 'package:free_chat/theme.dart';
 
 void main() {
@@ -7,14 +8,14 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({ Key? key }) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'free chat',
       debugShowCheckedModeBanner: false,
       darkTheme: darkThemeData(context),
-      home: const Text('This is home screen.')
+      home: const WelcomeScreen()
     );
   }
 }
--- lib/screens/welcome/welcome_screen.dart
+++ lib/screens/welcome/welcome_screen.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
 class WelcomeScreen extends StatelessWidget {
-  const WelcomeScreen(Key? key) : super(key: key);
+  const WelcomeScreen({ Key? key }) : super(key: key);
   
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container()
+    );
   }
 }

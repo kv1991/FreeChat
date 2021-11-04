1885e8ef9fb6077227255bf323319448875b6074
Kaiwen Tian <kaiwen.tian@wishlife.com>
2021-11-04 22:26:51 +0800

    feat:  built welcome screen UI.

--- lib/screens/welcome/welcome_screen.dart
+++ lib/screens/welcome/welcome_screen.dart
@@ -6,7 +6,45 @@ class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container()
+      body: SafeArea(
+        child: Column(
+          children: [
+            const Spacer(flex: 2),
+            Image.asset('assets/images/welcome_image.png'),
+            const Spacer(flex: 3),
+            Text(
+              'Welcome to our freedom messaging app',
+              style: Theme.of(context).textTheme.headline5?.copyWith(fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+            ),
+            const Spacer(),
+            Text(
+              'Freedom talk any person of your \nmother language.',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Theme.of(context).textTheme.bodyText1?.color?.withOpacity(0.64)
+              )
+            ),
+            const Spacer(flex: 3),
+            TextButton(
+              onPressed: () {
+                print('wewe');
+              },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Skip', style: TextStyle(color: Theme.of(context).textTheme.bodyText1!.color!.withOpacity(0.8))),
+                  Icon(
+                    Icons.arrow_forward_ios,
+                    size: 14,
+                    color: Theme.of(context).textTheme.bodyText1!.color!.withOpacity(0.9)
+                  )
+                ]
+              ),
+            )
+          ]
+        ),
+      )
     );
   }
 }

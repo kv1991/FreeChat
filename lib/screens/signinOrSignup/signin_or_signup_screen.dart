75ba465ac86f46cf31d2c405b9f97c42a21dac98
Kaiwen Tian <kaiwen.tian@wishlife.com>
2021-11-06 21:47:58 +0800

    feat: built sign screen.

--- lib/screens/signinOrSignup/signin_or_signup_screen.dart
+++ lib/screens/signinOrSignup/signin_or_signup_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:free_chat/constants.dart';
 
 class SigninOrSignupScreen extends StatelessWidget {
   const SigninOrSignupScreen({ Key? key }) : super(key: key);
@@ -6,7 +7,55 @@ class SigninOrSignupScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container()
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+          child: Column(
+            children: [
+              const Spacer(flex: 2),
+              Image.asset(
+                'assets/images/Logo_light.png',
+                height: 146,
+              ),
+              const Spacer(),
+              MaterialButton(
+                onPressed: () {},
+                child: Container(
+                  width: double.infinity,
+                  padding: const EdgeInsets.symmetric(vertical: kDefaultPadding * 0.75),
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(40)),
+                    color: kPrimaryColor,
+                  ),
+                  child: const Text(
+                    'Sign In',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.white)
+                    ),
+                )
+              ),
+              const SizedBox(height: kDefaultPadding * 1.5),
+              MaterialButton(
+                onPressed: () {},
+                child: Container(
+                  width: double.infinity,
+                  padding: const EdgeInsets.symmetric(vertical: kDefaultPadding * 0.75),
+                  decoration: BoxDecoration(
+                    borderRadius: const BorderRadius.all(Radius.circular(40)),
+                    color: Theme.of(context).colorScheme.secondary
+                  ),
+                  child: const Text(
+                    'Sign Up',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.white)
+                    ),
+                )
+              ),
+              const Spacer(flex: 2),
+            ]
+          )
+        )
+      )
     );
   }
 }

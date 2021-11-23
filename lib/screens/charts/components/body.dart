c962f047e55e69e3722dd2a9866a1bd3aa654f01
Kaiwen Tian <kaiwen.tian@wishlife.com>
2021-11-23 14:48:11 +0800

    feat:  built active chat icon.

--- lib/screens/charts/components/body.dart
+++ lib/screens/charts/components/body.dart
@@ -26,14 +26,14 @@ class Body extends StatelessWidget {
               )
             ],
           ),
-          decoration: BoxDecoration(
+          decoration: const BoxDecoration(
             color: kPrimaryColor
           )
         ),
         Expanded(
           child: ListView.builder(
             itemCount: chatsData.length,
-            itemBuilder: (context, index) => ChatCard(chat: chatsData[index])
+            itemBuilder: (context, index) => ChatCard(chat: chatsData[index], press: () {})
           ),
           // child: Column(
           //   children: [
@@ -48,56 +48,82 @@ class Body extends StatelessWidget {
 
 class ChatCard extends StatelessWidget {
   final Chat? chat;
+  final VoidCallback press;
 
   const ChatCard({
     Key? key,
-    this.chat
+    this.chat,
+    required this.press
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(
-        horizontal: kDefaultPadding,
-        vertical: kDefaultPadding * 0.75
-      ),
-      child: Row(
-        children: [
-          CircleAvatar(
-            radius: 24,
-            backgroundImage: AssetImage(chat!.image)
-          ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    chat!.name,
-                    style: const TextStyle(
-                      fontWeight: FontWeight.w500,
-                      fontSize: 16
+    return InkWell(
+      onTap: press,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: kDefaultPadding,
+          vertical: kDefaultPadding * 0.75
+        ),
+        child: Row(
+          children: [
+            Stack(
+              children: [
+                CircleAvatar(
+                  radius: 24,
+                  backgroundImage: AssetImage(chat!.image)
+                ),
+                if(chat!.isActive)
+                Positioned(
+                  right: 0,
+                  bottom: 0,
+                  child: Container(
+                    width: 16,
+                    height: 16,
+                    decoration: BoxDecoration(
+                      color: kPrimaryColor,
+                      shape: BoxShape.circle,
+                      border: Border.all(
+                        color: Theme.of(context).scaffoldBackgroundColor,
+                        width: 3
+                      )
                     )
                   ),
-                  const SizedBox(height: 8),
-                  Opacity(
-                    opacity: 0.64,
-                    child: Text(
-                      chat!.lastMessage,
-                      maxLines: 1,
+                )
+              ],
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      chat!.name,
                       style: const TextStyle(
                         fontWeight: FontWeight.w500,
-                        fontSize: 14
+                        fontSize: 16
                       )
                     ),
-                  )
-                ]
+                    const SizedBox(height: 8),
+                    Opacity(
+                      opacity: 0.64,
+                      child: Text(
+                        chat!.lastMessage,
+                        maxLines: 1,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 14
+                        )
+                      ),
+                    )
+                  ]
+                ),
               ),
             ),
-          ),
-          Text(chat!.time)
-        ]
+            Text(chat!.time)
+          ]
+        ),
       ),
     );
   }

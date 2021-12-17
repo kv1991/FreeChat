a96619a8002467482d3440437f584eaccd3a26f1
Kaiwen Tian <kaiwen.tian@wishlife.com>
2021-12-18 07:44:13 +0800

    feat:  added text message content.

--- lib/screens/messages/components/body.dart
+++ lib/screens/messages/components/body.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:free_chat/constants.dart';
+import 'package:free_chat/models/chat_message.dart';
 import 'package:free_chat/screens/messages/components/chat_input_field.dart';
 
 class Body extends StatelessWidget {
@@ -8,9 +10,47 @@ class Body extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        const Spacer(),
-        ChatInputField()
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(kDefaultPadding),
+            child: ListView.builder(
+              itemCount: demeChatMessages.length,
+              itemBuilder: (context, index) => Message(message: demeChatMessages[index])
+            ),
+          )
+        ),
+        const ChatInputField()
       ]
+    );
+  }
+}
+
+class Message extends StatelessWidget {
+  final ChatMessage message;
+  const Message({ Key? key, required this.message }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: message.isSender ? MainAxisAlignment.end : MainAxisAlignment.start,
+      children: [
+        Container(
+          padding: const EdgeInsets.symmetric(
+            horizontal: kDefaultPadding * 0.75,
+            vertical: kDefaultPadding * 0.5 
+          ),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30),
+            color: message.isSender ? kPrimaryColor : kPrimaryColor.withOpacity(0.08)
+          ),
+          child: Text(
+            message.text,
+            style: TextStyle(
+              color: message.isSender ? Colors.white : Theme.of(context).textTheme.bodyText1?.color
+            )
+          )
+        ),
+      ],
     );
   }
 }
